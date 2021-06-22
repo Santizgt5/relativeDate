a336638371346f25fe2c2658d6699e8b187d24a8
Santizgt5 <39011323+Santizgt5@users.noreply.github.com>
2021-06-21 21:22:08 -0500

    Primer commit

---
+++ src/main/java/com/programita/relativeDate/RelativeDateApplication.java
@@ -0,0 +1,13 @@
+package com.programita.relativeDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelativeDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelativeDateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programita/relativeDate/RelativeDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programita.relativeDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelativeDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
